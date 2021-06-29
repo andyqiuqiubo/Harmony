c894dc586720ad4d4fadbe8760836a7e31d662f1
andyqiubo <andyqiubo@126.com>
2021-06-29 19:49:29 +0800

    harmony

---
+++ entry/src/main/java/com/andyqiubo/helloharmony/MyApplication.java
@@ -0,0 +1,10 @@
+package com.andyqiubo.helloharmony;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
